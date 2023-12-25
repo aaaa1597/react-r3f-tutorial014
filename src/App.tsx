ad6230653c451175d7d61c221086583856752079
jun <jun@Ubt2200.aaaaa>
2023-12-25 20:17:57 +0900

    初回コミット

---
+++ src/App.tsx
@@ -0,0 +1,36 @@
+import React, {useRef} from 'react';
+import './App.css';
+import { Canvas, useFrame, MeshProps  } from '@react-three/fiber'
+import * as THREE from 'three'
+
+const Box = (props: MeshProps) => {
+  const ref = useRef<THREE.Mesh>(null!)
+
+  useFrame((_, delta) => {
+    if( !ref.current) return
+    ref.current.rotation.x += 1 * delta
+    ref.current.rotation.y += 0.5 * delta
+  })
+
+  return (
+    <mesh {...props} ref={ref}>
+      <boxGeometry />
+      <meshBasicMaterial color={0x00ff00} wireframe />
+    </mesh>
+  )
+}
+
+const App = () => {
+  return (
+    <div style={{ width: "75vw", height: "75vh" }}>
+      <Canvas camera={{ position: [3, 1, 2] }}>
+        <ambientLight />
+        <directionalLight />
+        <Box position={[-0.75, 0, 0]} name="A" />
+        <Box position={[0.75, 0, 0]} name="B" />
+      </Canvas>
+    </div>
+  );
+}
+
+export default App;
